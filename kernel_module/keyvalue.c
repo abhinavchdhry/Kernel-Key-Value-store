8d10dbe1836ef206e227209cb88227b684b2af7f
abhinavchdhry <abhinav155.bits@gmail.com>
2016-11-06 21:35:19 -0500

    Adding actual project changes to keyvalue.c to implement the working logic of the key value store

--- kernel_module/keyvalue.c
+++ kernel_module/keyvalue.c
@@ -22,7 +22,9 @@
 //
 ////////////////////////////////////////////////////////////////////////
 //
-//   Author:  Hung-Wei Tseng
+//   Authors:  Hung-Wei Tseng
+//				Abhinav Choudhury (Student ID: 200159347)
+//				Aditya Virmani	(Student ID: 200068593)
 //
 //   Description:
 //     Skeleton of KeyValue Pseudo Device
@@ -41,8 +43,67 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <linux/poll.h>
+#include <linux/string.h>
+#include <linux/semaphore.h>
+#include <linux/mutex.h>
 
-unsigned transaction_id;
+#define HASHTABLESIZE (1024)
+#define HASHKEYMASK (0x3ff)
+#define TOP54MASK (0xfffffffffffffc00)
+#define MAX_READERS (10)
+
+/*
+	We use a hash-table with Linked Lists for conflict resolution.
+	Currently, the size of the hash-table is given by HASHTABLESIZE which is 1024
+	The hash-function used is a bit-mask for the lower 10 bits of the key (given by HASHKEYMASK)
+	TOP54MASK is an octal mask for the remaining upper 54 bits of the 64-bit key
+	We limit the maximum number of readers that can simultaneously read from the key-value store to MAX_READERS = 10
+
+	LOCKING MECHANISM:
+	We use a single mutex "transaction_lock" to synchronize updates to transaction_id
+	The "update_lock" is a global write-lock
+	Using the write lock we have a reader lock "r_lock"
+
+	The rest, as they say, is history. Good luck!
+*/
+
+volatile unsigned transaction_id;
+typedef struct mutex mutex_t;
+mutex_t transaction_lock;
+mutex_t update_lock;
+
+struct read_lock {
+	mutex_t read_sem;
+	int readers;
+} r_lock;
+
+struct hash_node {
+	u64 top54_bits;
+	void* data;
+	u64 datasize;
+	struct hash_node* next;
+};
+
+struct hash_node* KV_HashTable[HASHTABLESIZE];
+
+static void acquire_read_lock(struct read_lock* r_lock)
+{
+	mutex_lock(&(r_lock->read_sem));
+	r_lock->readers += 1;
+	if (r_lock->readers == 1)
+		mutex_lock(&update_lock);
+	mutex_unlock(&(r_lock->read_sem));
+}
+
+static void release_read_lock(struct read_lock* r_lock)
+{
+	mutex_lock(&(r_lock->read_sem));
+	r_lock->readers -= 1;
+	if (r_lock->readers == 0)
+		mutex_unlock(&update_lock);
+	mutex_unlock(&(r_lock->read_sem));
+}
+
 static void free_callback(void *data)
 {
 }
@@ -50,22 +111,206 @@ static void free_callback(void *data)
 static long keyvalue_get(struct keyvalue_get __user *ukv)
 {
     struct keyvalue_get kv;
+	u64 hashkey = (ukv->key & HASHKEYMASK);
+	u64 topbits = ((ukv->key & TOP54MASK) >> 10);	
+	if (KV_HashTable[hashkey] == NULL)
+	{
+		return -1;
+	}
+	else
+	{
+		acquire_read_lock(&r_lock);
+		struct hash_node* iter = KV_HashTable[hashkey];
 
-    return transaction_id++;
+		while (iter->top54_bits != topbits && iter != NULL)
+			iter = iter->next;
+
+		if (iter != NULL)	// We have found a key-value pair corresponding to the given key
+		{
+			*(ukv->size) = iter->datasize;
+			memcpy((char*)(ukv->data), (char*)(iter->data), iter->datasize);
+		}
+		else	// No key-value entry was found
+		{
+			release_read_lock(&r_lock);
+			return -1;
+		}
+
+		release_read_lock(&r_lock);
+	}
+
+	mutex_lock(&transaction_lock);
+	transaction_id++;
+	mutex_unlock(&transaction_lock);
+
+    return transaction_id;
 }
 
 static long keyvalue_set(struct keyvalue_set __user *ukv)
 {
     struct keyvalue_set kv;
+	u64 hashkey = (ukv->key & HASHKEYMASK);
+	u64 topbits = ((ukv->key & TOP54MASK) >> 10);
 
-    return transaction_id++;
+	mutex_lock(&update_lock);
+
+	if (KV_HashTable[hashkey] == NULL)	// There is nothing there for this key. Create a new entry
+	{
+		KV_HashTable[hashkey] = kmalloc(sizeof(struct hash_node), GFP_KERNEL);
+		if (KV_HashTable[hashkey] == NULL)
+		{
+			printk("Kernel allocation failure!\n");	
+			mutex_unlock(&update_lock);
+			return -1;
+		}
+		(KV_HashTable[hashkey])->top54_bits = topbits;
+		(KV_HashTable[hashkey])->datasize = ukv->size;
+		(KV_HashTable[hashkey])->data = kmalloc(ukv->size, GFP_KERNEL);
+		if (KV_HashTable[hashkey]->data == NULL)
+		{
+			printk(KERN_ERR "Kernel allocation failure!\n");
+			kfree(KV_HashTable[hashkey]);
+			mutex_unlock(&update_lock);
+			return -1;
+		}
+		(KV_HashTable[hashkey])->next = NULL;
+		memcpy((char*)(KV_HashTable[hashkey])->data, (char*)(ukv->data), ukv->size);
+	}
+	else
+	{
+		// First check if a key-value pair for the given key already exists
+		// If so, update it
+		struct hash_node* last = KV_HashTable[hashkey];
+		while (last->top54_bits != topbits && last->next != NULL)
+			last = last->next;
+
+		if (last->top54_bits == topbits) // We already have an entry
+		{
+			last->datasize = ukv->size;
+			void* old_data = last->data;
+			last->data = kmalloc(ukv->size, GFP_KERNEL);
+			if (last->data == NULL)
+			{
+				printk(KERN_ERR "Kernel allocation failure!\n");
+				last->data = old_data;
+				mutex_unlock(&update_lock);
+				return -1;				
+			}
+			memcpy((char*)(last->data), (char*)(ukv->data), ukv->size);
+			kfree(old_data);
+		}
+		else if (last->next == NULL)	// Nah, we didn't find a match. Create a new entry
+		{
+			last->next = kmalloc(sizeof(struct hash_node), GFP_KERNEL);
+			if (last->next == NULL)
+			{
+				printk(KERN_ERR "Kernel allocation failure!\n");
+				mutex_unlock(&update_lock);
+				return -1;
+			}
+			struct hash_node* prev = last;
+			last = last->next;
+	
+			last->top54_bits = topbits;
+			last->datasize = ukv->size;
+			last->next = NULL;
+			last->data = kmalloc(ukv->size, GFP_KERNEL);
+			if (last->data == NULL)
+			{
+				printk(KERN_ERR "Kernel allocation failure!\n");
+				kfree(last);
+				prev->next = NULL;
+				mutex_unlock(&update_lock);
+				return -1;
+			}
+			memcpy((char*)(last->data), (char*)(ukv->data), ukv->size);
+		}
+		else	// This is not supposed to happen!
+		{
+			mutex_unlock(&update_lock);
+			printk(KERN_ERR "Oops! Not supposed to happen...!\n");
+			return -1;
+		}
+	}
+
+	mutex_unlock(&update_lock);
+
+	mutex_lock(&transaction_lock);
+	transaction_id++;
+	mutex_unlock(&transaction_lock);
+	
+    return transaction_id;
 }
 
 static long keyvalue_delete(struct keyvalue_delete __user *ukv)
 {
     struct keyvalue_delete kv;
+	u64 hashkey = (ukv->key & HASHKEYMASK);
+	u64 topbits = ((ukv->key & TOP54MASK) >> 10);
 
-    return transaction_id++;
+	mutex_lock(&update_lock);
+
+	if (KV_HashTable[hashkey] == NULL)	// No such entry for key
+	{
+		mutex_unlock(&update_lock);
+		return -1;
+	}
+	else
+	{
+		struct hash_node* last = KV_HashTable[hashkey];
+		while (last->top54_bits != topbits && last->next != NULL)
+			last = last->next;
+		
+		if (last->top54_bits == topbits) // We already have an entry
+		{
+			// Remove the node from the Hash Table linked list
+			struct hash_node* prev = KV_HashTable[hashkey];
+
+			if (prev == last)	// Entry is at head of LL
+			{
+				// First free the memory for the value data
+				kfree(prev->data);
+
+				// Now free the linked list node
+				KV_HashTable[hashkey] = prev->next;
+				kfree(prev);
+				prev = last = NULL;
+			}
+			else	// Entry is somewhere else in the LL
+			{
+				while (prev->next != last)
+					prev = prev->next;
+
+				// Free the memory for value data in this node
+				kfree(last->data);
+
+				// Unhook this node from LL
+				prev->next = last->next;
+				last->next = NULL;
+				kfree(last);
+				last = NULL;
+			}
+		}
+		else if (last->next == NULL)
+		{
+			mutex_unlock(&update_lock);
+			return -1;
+		}
+		else	// This is not supposed to happen!
+		{
+			mutex_unlock(&update_lock);
+			printk(KERN_ERR "Oops! Not supposed to happen...!\n");
+			return -1;
+		}
+	}
+
+	mutex_unlock(&update_lock);
+
+	mutex_lock(&transaction_lock);
+	transaction_id++;
+	mutex_unlock(&transaction_lock);
+
+    return transaction_id;
 }
 
 //Added by Hung-Wei
@@ -110,12 +355,24 @@ static struct miscdevice keyvalue_dev = {
     .fops = &keyvalue_fops,
 };
 
+static void initialize_locks(void)
+{
+	mutex_init(&transaction_lock);
+
+	r_lock.readers = 0;
+
+	mutex_init(&r_lock.read_sem);
+	mutex_init(&update_lock);
+}
+
 static int __init keyvalue_init(void)
 {
     int ret;
 
     if ((ret = misc_register(&keyvalue_dev)))
         printk(KERN_ERR "Unable to register \"keyvalue\" misc device\n");
+
+    initialize_locks();
     return ret;
 }
 
